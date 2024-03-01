936148fcc47728f69abcd423bbf615f50b60725f
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-03-01 22:22:08 +0500

    cors added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 import { connectDB } from "./config/dbConn.js";
 import authRouter from "./Routes/authRoute.js";
@@ -8,6 +9,7 @@ config();
 
 const port = process.env.PORT || 8000;
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 
